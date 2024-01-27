d26a032cacf71f0c0e08c8d3d5e002ed3e2cb142
MchllD <mich11dimafelix@gmail.com>
2024-01-27 22:16:38 +0800

    pseudocode

---
+++ Exercise13.py
@@ -0,0 +1,12 @@
+# Exercise 13: Print multiplication table from 1 to 10
+
+# pseudocode
+
+
+# create  outer loop for rows (1 to 10)
+# create Inner loop for columns (1 to 10)
+# Calculate the product of i and j
+# Print the result with adjusted width for proper alignment
+
+
+# ____________________________ actual code _________________________________________________
